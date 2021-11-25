4f6d472931030874cbca06b696bc1889ab2a520d
Fernando Matsumoto <ferkmatsumoto@gmail.com>
2021-11-25 17:58:44 +0100

    Fix terminal coloring bug

--- aoc_cli/src/terminal_backend.rs
+++ aoc_cli/src/terminal_backend.rs
@@ -28,6 +28,7 @@ impl TerminalOutputBackend {
         stdout.set_color(ColorSpec::new().set_fg(Some(Color::Red)))?;
         self.write_block(&mut stdout, &format!("{:?}", err))?;
         writeln!(stdout)?;
+        stdout.reset()?;
         Ok(())
     }
 }
@@ -35,12 +36,13 @@ impl TerminalOutputBackend {
 impl aoc::ProblemOutputBackend for TerminalOutputBackend {
     fn start(&mut self, spec: &InputSpec) -> aoc::Result<()> {
         let mut stdout = StandardStream::stdout(self.color_choice);
-        stdout.reset()?;
         stdout.set_color(ColorSpec::new().set_bold(true))?;
 
         write!(stdout, "Year {}, day {}", spec.id.year, spec.id.day)?;
         write!(stdout, " ({})", spec.variant)?;
         writeln!(stdout)?;
+
+        stdout.reset()?;
         Ok(())
     }
 
@@ -51,12 +53,13 @@ impl aoc::ProblemOutputBackend for TerminalOutputBackend {
         solution: &dyn Display,
     ) -> aoc::Result<()> {
         let mut stdout = StandardStream::stdout(self.color_choice);
-        stdout.reset()?;
         write!(stdout, "    [part {}] ", part)?;
         self.write_block(&mut stdout, solution)?;
 
         stdout.set_color(ColorSpec::new().set_dimmed(true))?;
         writeln!(stdout, "    (finished in {:.1?})", exec_time)?;
+
+        stdout.reset()?;
         Ok(())
     }
 }
