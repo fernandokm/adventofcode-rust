034381b392df8200893fb22350bebd2d96719bfa
Fernando Matsumoto <ferkmatsumoto@gmail.com>
2021-12-09 20:33:31 +0100

    Add 2021.{3,4}

---
+++ src/year2021/day3.rs
@@ -0,0 +1,58 @@
+use anyhow::Context;
+use aoc::ProblemOutput;
+use itertools::Itertools;
+
+aoc::register!(solve, 2021, 3);
+
+pub fn solve(input: &str, out: &mut ProblemOutput) -> anyhow::Result<()> {
+    let n_bits = input.trim().lines().next().context("empty input")?.len();
+    let nums: Vec<u32> = input
+        .trim()
+        .lines()
+        .map(|line| u32::from_str_radix(line, 2))
+        .try_collect()?;
+
+    let gamma = (0..n_bits)
+        .map(|i| most_common_bit(i, &nums) << i)
+        .sum::<u32>();
+    let epsilon = (!gamma) & ((1 << n_bits) - 1);
+
+    out.set_part1(gamma * epsilon);
+
+    let o2_rating = filter_part_2(true, n_bits, &nums).context("no O2 rating found")?;
+    let co2_rating = filter_part_2(false, n_bits, &nums).context("no CO2 rating found")?;
+
+    out.set_part2(o2_rating * co2_rating);
+
+    Ok(())
+}
+
+fn most_common_bit(pos: usize, nums: &[u32]) -> u32 {
+    let bit_at_pos = 1 << pos;
+    let num_bits_1 = nums.iter().filter(|&&n| n & bit_at_pos != 0).count();
+    if num_bits_1 * 2 >= nums.len() {
+        1
+    } else {
+        0
+    }
+}
+
+fn filter_part_2(keep_most_common: bool, n_bits: usize, nums: &[u32]) -> Option<u32> {
+    let mut nums = nums.iter().copied().collect_vec();
+    for pos in (0..n_bits).rev() {
+        let desired_value = if keep_most_common {
+            most_common_bit(pos, &nums) << pos
+        } else {
+            (most_common_bit(pos, &nums) ^ 1) << pos
+        };
+        nums = nums
+            .iter()
+            .copied()
+            .filter(|&a| a & (1 << pos) == desired_value)
+            .collect();
+        if nums.len() == 1 {
+            return Some(nums[0]);
+        }
+    }
+    None
+}
---
+++ src/year2021/day4.rs
@@ -0,0 +1,83 @@
+use std::str::FromStr;
+
+use anyhow::Context;
+use aoc::ProblemOutput;
+use itertools::Itertools;
+
+aoc::register!(solve, 2021, 4);
+
+pub fn solve(input: &str, out: &mut ProblemOutput) -> anyhow::Result<()> {
+    let mut blocks = input.trim().split("\n\n");
+    let numbers: Vec<u32> = blocks
+        .next()
+        .context("invalid input")?
+        .split(',')
+        .map(|x| x.parse())
+        .try_collect()?;
+
+    let mut boards: Vec<Board> = blocks.map(FromStr::from_str).try_collect()?;
+    let mut scores = Vec::with_capacity(boards.len());
+    for &n in &numbers {
+        for board in &mut boards {
+            if let Some(score) = board.update(n) {
+                scores.push(score)
+            }
+        }
+    }
+
+    out.set_part1(scores.first().unwrap());
+    out.set_part2(scores.last().unwrap());
+
+    Ok(())
+}
+
+struct Board {
+    g: Vec<Vec<u32>>,
+    done: bool,
+}
+
+impl FromStr for Board {
+    type Err = anyhow::Error;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let g = s
+            .trim()
+            .lines()
+            .map(|line| line.split_whitespace().map(FromStr::from_str).collect())
+            .try_collect()?;
+        Ok(Board { g, done: false })
+    }
+}
+
+impl Board {
+    fn update(&mut self, n: u32) -> Option<u32> {
+        if self.done {
+            return None;
+        }
+
+        let (i, j) = self.find_pos(n)?;
+        self.g[i][j] = 0;
+        if self.g[i].iter().all(|&k| k == 0) || self.g.iter().all(|row| row[j] == 0) {
+            self.done = true;
+            Some(self.score(n))
+        } else {
+            None
+        }
+    }
+
+    fn find_pos(&self, n: u32) -> Option<(usize, usize)> {
+        self.g.iter().enumerate().find_map(|(i, row)| {
+            row.iter()
+                .enumerate()
+                .find_map(|(j, &m)| if n == m { Some((i, j)) } else { None })
+        })
+    }
+
+    fn score(&self, n: u32) -> u32 {
+        n * self
+            .g
+            .iter()
+            .map(|row| row.iter().sum::<u32>())
+            .sum::<u32>()
+    }
+}
